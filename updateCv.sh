9dc0c588801cb76b201b07cb35c9f668d71b64db
Dmitry Polishuk <dp@neosphere.com>
2014-09-26 18:14:15 +0400

    updated CV

--- updateCv.sh
+++ updateCv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp -rf ~/Downloads/Dmitry_Polischuk.odt.docx ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.docx
-cp -rf ~/Downloads/Dmitry_Polischuk.odt.odt ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.odt
-cp -rf ~/Downloads/Dmitry_Polischuk.odt.pdf ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.pdf
-cp -rf ~/Downloads/Dmitry_Polischuk.odt.zip ~/work/dpolishuk.github.io/Dmitry_Polischuk.odt.zip
+cp -rf ~/Documents/Dmitry_Polischuk.docx ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.docx
+cp -rf ~/Documents/Dmitry_Polischuk.odt ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.odt
+cp -rf ~/Documents/Dmitry_Polischuk.pdf ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.pdf
+cp -rf ~/Documents/Dmitry_Polischuk.zip ~/work/dpolishuk.github.io/Dmitry_Polischuk.odt.zip
 pushd
 cd ~/work/dpolishuk.github.io && unzip Dmitry_Polischuk.odt.zip && cp -rf Dmitry_Polischuk.odt.html Dmitry_Polishuk_CV.html
 rm -rf Dmitry_Polischuk.odt.zip Dmitry_Polischuk.odt.html
