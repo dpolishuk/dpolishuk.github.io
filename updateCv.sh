bf7ca8716e3aef67b85c1891f5f3dff3111b4c39
Dmitry Polishuk <dmitry.polishuk@gmail.com>
2015-03-02 23:20:04 +0300

    added new place of job

--- updateCv.sh
+++ updateCv.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-cp -rf ~/Documents/Dmitry_Polischuk.docx ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.docx
-cp -rf ~/Documents/Dmitry_Polischuk.odt ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.odt
-cp -rf ~/Documents/Dmitry_Polischuk.pdf ~/work/dpolishuk.github.io/Dmitry_Polishuk_CV.pdf
-cp -rf ~/Documents/Dmitry_Polischuk.zip ~/work/dpolishuk.github.io/Dmitry_Polischuk.odt.zip
-pushd
-cd ~/work/dpolishuk.github.io && unzip Dmitry_Polischuk.odt.zip && cp -rf Dmitry_Polischuk.odt.html Dmitry_Polishuk_CV.html
-rm -rf Dmitry_Polischuk.odt.zip Dmitry_Polischuk.odt.html
+curl -X GET "https://docs.google.com/document/export?format=docx&id=12NVEphzM1gfzCfQn7_0BIc3rUNdu1OFWwuqcA-nk25o" > Dmitry_Polishuk_CV.docx
+curl -X GET "https://docs.google.com/document/export?format=odt&id=12NVEphzM1gfzCfQn7_0BIc3rUNdu1OFWwuqcA-nk25o" > Dmitry_Polishuk_CV.odt
+curl -X GET "https://docs.google.com/document/export?format=pdf&id=12NVEphzM1gfzCfQn7_0BIc3rUNdu1OFWwuqcA-nk25o" > Dmitry_Polishuk_CV.pdf
+curl -X GET "https://docs.google.com/document/export?format=html&id=12NVEphzM1gfzCfQn7_0BIc3rUNdu1OFWwuqcA-nk25o" > Dmitry_Polishuk_CV.html
